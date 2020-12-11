94700b6b194e5a5bc2a9c850b4239128a31a8e4f
julio.oliveira <jcalvesoliveira@hotmail.com>
2020-12-11 15:54:38 -0300

    adding communication with statistics service

--- machinelearningteam/summary-statistics-service/pkg/api/api.go
+++ machinelearningteam/summary-statistics-service/pkg/api/api.go
@@ -3,8 +3,15 @@ package api
 import (
 	"context"
 	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
 
 	api "github.com/e-conomic/hiring-assignments/machinelearningteam/summary-statistics-service/proto"
+)
+
+const (
+	host = "localhost:50050"
 )
 
 // Server is a server implementing the proto API
@@ -19,7 +26,21 @@ func (s *Server) SummarizeDocument(
 ) (*api.SummarizeDocumentReply, error) {
 	// Echo
 	fmt.Println("Received document...")
+
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := api.NewStatisticsProcesserClient(conn)
+
+	doc_content := req.Document.GetContent()
+	resp, err := client.ProcessDocument(context.Background(), &api.ProcessDocumentRequest{
+		Content: doc_content,
+	},
+	)
+
 	return &api.SummarizeDocumentReply{
-		Content: req.Document.GetContent(),
+		Content: resp.Summary,
 	}, nil
 }
